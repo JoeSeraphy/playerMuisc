83566ead5b02fd030b3dae4964f072a67749c9c2
Joelson Silva <joelson.s@outlook.com>
2022-07-13 13:46:52 -0300

    adicionando mais duas musicas

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+let musics = [
+  {title: 'Running Up That Hill', 
+  artits: 'Kate Bush', 
+  src: 'assets/Kate Bush - Running Up That Hill - Official Music Video.mp3',
+  img: 'assets/max.png'
+    },
+
+  {title: 'Master Of Puppets', 
+  artits: 'Metallica', 
+  src: 'assets/Metallica - Master of Puppets.mp3',
+  img: 'assets/eddie.png'
+    },
+
+  {title: 'Sunglesses At Night',
+  artits: 'Corey Hart',
+  src: 'assets/Corey Hart - Sunglasses At Night.mp3',
+  img: 'assets/steve.png'}
+]
+
 let music = document.querySelector('audio')
+let indexMusic = 0
+let durationMusic = document.querySelector('.fim')
+let image = document.querySelector('.discricao img')
+let musicName = document.querySelector('.discricao h2')
+let artistName = document.querySelector('.discricao p')
+
+renderMusic(indexMusic)
+
+
 
 document.querySelector('.play').addEventListener('click', playMusic)
 document.querySelector('.pause').addEventListener('click', pauseMusic)
 
 music.addEventListener('timeupdate', upBarra)
+
+document.querySelector('.skipBack').addEventListener('click', () => {
+    indexMusic--
+    if (indexMusic < 0) {
+      indexMusic = 2
+    }
+    renderMusic(indexMusic)
+})
+
+document.querySelector('.skipNext').addEventListener('click', () => {
+  indexMusic++
+  if (indexMusic > 2) {
+    indexMusic = 0
+  }
+  renderMusic(indexMusic)
+})
 
 function playMusic() {
   music.play()
@@ -37,3 +81,13 @@ function segundoParaMinutos(segundos) {
 let endMusic = document.querySelector('.fim')
 
 endMusic.textContent = segundoParaMinutos(Math.floor(music.duration))
+
+function renderMusic(index) {
+    music.setAttribute('src', musics[index].src);
+    music.addEventListener('loadeddata', () => {
+      musicName.textContent = musics[index].title;
+      artistName.textContent = musics[index].artits;
+      image.src = musics[index].img;
+      durationMusic.textContent = segundoParaMinutos(Math.floor(music.duration))
+    })
+}
